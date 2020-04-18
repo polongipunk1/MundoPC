528d421508ed24cf5d33f2749ae87b89a17eb473
Jerry Anthony Gabriel Jacuinde <bandoler71@gmail.com>
2020-04-18 14:58:20 -0500

    sitio_asp

---
+++ Mundo_PC/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mundo_PC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult productos()
+        {
+            return View();
+        }
+        public ActionResult mis_compras()
+        {
+            return View();
+        }
+        public ActionResult carrito()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult registro()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mundo_PC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mundo_PC.MvcApplication" Language="C#" %>
